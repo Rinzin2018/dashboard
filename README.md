5c6f611a53e8f951e615b2162e5e8c220389a3f3
rinzin <rinzin.norbu@dcpl.bt>
2020-06-21 13:42:19 +0600

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# React Dashboard Template clone from react sass template
+
+Your browser should now open and show the app. Otherwise open http://localhost:3000/ in your browser. Editing files will automatically refresh the page.
+
+## Deployment
+
+If you are happy with the state of your website you can run:
+
+```
+npm run build 
+```
+
+It will create a folder named build with your compiled project inside. After that copy its content into your webroot and you are ready to go.
+
+## Build With
+
+* [Create-React-App](https://github.com/facebook/create-react-app) - Used to bootstrap the development
+* [Material-UI](https://github.com/mui-org/material-ui) - Material Design components
+* [React-Router](https://github.com/ReactTraining/react-router) - Routing of the app
+* [Pace](https://github.com/HubSpot/pace) - Loading bar at the top
+* [Emoji-Mart](https://github.com/missive/emoji-mart) - Picker for the emojis
+* [React-Dropzone](https://github.com/react-dropzone/react-dropzone) - File drop component for uploads
+* [Recharts](https://github.com/recharts/recharts) - Charting library I used for the statistics
+* [Aos](https://github.com/michalsnik/aos) - Animations based on viewport
+* [React-Cropper](https://github.com/roadmanfong/react-cropper) - Cropper for the image uploads
+* [React-Stripe-js](https://github.com/stripe/react-stripe-js) - Stripes payment elements
---
+++ src/logged_out/components/home/FeatureSection.js
@@ -0,0 +1,148 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Grid, Typography, isWidthUp, withWidth } from "@material-ui/core";
+import CodeIcon from "@material-ui/icons/Code";
+import BuildIcon from "@material-ui/icons/Build";
+import ComputerIcon from "@material-ui/icons/Computer";
+import BarChartIcon from "@material-ui/icons/BarChart";
+import HeadsetMicIcon from "@material-ui/icons/HeadsetMic";
+import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import CloudIcon from "@material-ui/icons/Cloud";
+import MeassageIcon from "@material-ui/icons/Message";
+import CancelIcon from "@material-ui/icons/Cancel";
+import calculateSpacing from "./calculateSpacing";
+import FeatureCard from "./FeatureCard";
+
+const iconSize = 30;
+
+const features = [
+  {
+    color: "#00C853",
+    headline: "Application Development",
+    text:
+      "Our expertise in e-commerce, adtech/martech, and people-to-people fields as well as other business areas helps " +
+        "us develop web and mobile solutions tailored with care for our clients. ",
+    icon: <BuildIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "0",
+    smDelay: "0"
+  },
+  {
+    color: "#6200EA",
+    headline: "Mobile Application Services\n",
+    text:
+      "To create a fast and smooth app for any platform, We Build product with an integrated application development " +
+        "model. This model gives a reasonable solution to control the budget and other resources. ",
+    icon: <CalendarTodayIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "200",
+    smDelay: "200"
+  },
+  {
+    color: "#0091EA",
+    headline: "Cloud Application Services\n",
+    text:
+      "Technology has vastly changed people’s expectations about the way their customers should interact with them." +
+        " Cloud can play a key role in facilitating a modern, effective experience for both citizens and public-sector workers.",
+    icon: <MeassageIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "400",
+    smDelay: "0"
+  },
+  {
+    color: "#d50000",
+    headline: "UI/UX Design Services",
+    text:
+      "Through powerful, well-designed user interfaces we help companies bring customers a seamless and enjoyable" +
+        " user experience which holds and deserves attention. ",
+    icon: <ComputerIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "0",
+    smDelay: "200"
+  },
+  {
+    color: "#DD2C00",
+    headline: "Digital Marketing Services",
+    text:
+      "We are proud to be at the forefront of digital marketing services in Bhutan, where we consistently deliver" +
+        " the results that brands need to be recognised as the best in the industry. ",
+    icon: <BarChartIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "200",
+    smDelay: "0"
+  },
+  {
+    color: "#64DD17",
+    headline: "AI & Big Data Services",
+    text:
+      "LAs Big Data technology is fast evolving, significant impediments still exist for an Enterprise to " +
+        "transform into a Data-driven business.",
+    icon: <HeadsetMicIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "400",
+    smDelay: "200"
+  },
+  {
+    color: "#304FFE",
+    headline: "Machine Learning",
+    text:
+      "Data and algorithms will be key drivers to gain competitive advantages in the coming decades. ",
+    icon: <CloudIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "0",
+    smDelay: "0"
+  },
+  {
+    color: "#C51162",
+    headline: "Feature 8",
+    text:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+    icon: <CodeIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "200",
+    smDelay: "200"
+  },
+  {
+    color: "#00B8D4",
+    headline: "Feature 9",
+    text:
+      "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et.",
+    icon: <CancelIcon style={{ fontSize: iconSize }} />,
+    mdDelay: "400",
+    smDelay: "0"
+  }
+];
+
+function FeatureSection(props) {
+  const { width } = props;
+  return (
+    <div style={{ backgroundColor: "#FFFFFF" }}>
+      <div className="container-fluid lg-p-top">
+        <Typography variant="h3" align="center" className="lg-mg-bottom">
+          Features
+        </Typography>
+        <div className="container-fluid">
+          <Grid container spacing={calculateSpacing(width)}>
+            {features.map(element => (
+              <Grid
+                item
+                xs={6}
+                md={4}
+                data-aos="zoom-in-up"
+                data-aos-delay={
+                  isWidthUp("md", width) ? element.mdDelay : element.smDelay
+                }
+                key={element.headline}
+              >
+                <FeatureCard
+                  Icon={element.icon}
+                  color={element.color}
+                  headline={element.headline}
+                  text={element.text}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+FeatureSection.propTypes = {
+  width: PropTypes.string.isRequired
+};
+
+export default withWidth()(FeatureSection);
